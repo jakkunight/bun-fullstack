bb1b13e570dd0a9f3433912eedf098b0a5b500d5
Jakku Night <J97k107u45k117n@gmail.com>
2023-09-11 11:00:32 -0400

    Base setup with Elysia-Prisma created.

---
+++ prisma/migrations/20230911134935_jakku/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE `Type` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `devices` VARCHAR(191) NOT NULL,
+    `balance` DECIMAL(65, 30) NOT NULL,
+    `currency` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_name_key`(`name`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Concept` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user` INTEGER NOT NULL,
+    `type` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user` INTEGER NOT NULL,
+    `type` INTEGER NOT NULL,
+    `amount` DECIMAL(65, 30) NOT NULL,
+    `concept` INTEGER NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,64 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new PrismaClient();
 
+const app = new Elysia();
+app.use(staticPlugin());
+app.get("/users/:id", async ({ params }) => {
+	try{
+		const {
+			id,
+			name,
+			email,
+			balance,
+			currency
+		} = await db.user.findUniqueOrThrow({
+			where: {
+				id: Number(params.id)
+			}
+		});
+		return {
+			id,
+			name,
+			email,
+			balance: Number(balance),
+			currency
+		};
+	}catch(err){
+		console.log(err);
+		return err;
+	}
+});
+app.post("/users/", async ({ body, request }) => {
+	try{
+		body.devices = request.headers.get("origin") || "";
+		body.password = await Bun.password.hash(body.password);
+		body.balance = Number(body.balance);
+
+		const result = db.user.create({
+			data: body as any
+		});
+		return result;
+	}catch(err){
+		return err;
+	}
+}, {
+	body: t.Object({
+		name: t.String(),
+		email: t.String(),
+		currency: t.String(),
+		password: t.String(),
+		balance: t.Union([
+			t.Number(),
+			t.String()
+		]),
+		devices: t.String()
+	})
+});
+
+app.listen(process.env.PORT || 3000);
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
